120c317051dd00871bff348827236dcda5e0aff9
zhaofeng-shu33 <616545598@qq.com>
2019-11-15 16:25:36 +0800

    add docs

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Empty documentation master file, created by
+   sphinx-quickstart on Wed Oct  9 17:41:44 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Empty's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- nvtc/io.h
+++ nvtc/io.h
@@ -1,11 +1,17 @@
 // Copyright 2019 zhaofeng-shu33
 #pragma once
+/// \file
+/// \brief input utility
+///
 #include <vector>
 #include <utility>
 #include <map>
 
 typedef std::vector< std::pair<int, int> > Edges;
-
+//! \brief construct the static graph from a given file
+//! \param[in,out] arcs the graph data structure which will be initialized,
+//! use free to release the memory
+//! \return (node_max_id, edge_num)
 std::pair<int, uint64_t> read_binfile_to_arclist(const char* file_name,
   int*& arcs);  // NOLINT(runtime/references)
 uint64_t get_split(uint64_t* arr, int arr_len, int split_num,
