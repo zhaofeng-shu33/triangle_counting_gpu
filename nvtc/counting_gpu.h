725da021d3528b47b9664ea25aefb1696d596c3f
zhaofeng-shu33 <616545598@qq.com>
2019-11-15 17:27:14 +0800

    add more comments

--- nvtc/counting_gpu.h
+++ nvtc/counting_gpu.h
@@ -5,7 +5,11 @@
 
 #include "nvtc/io.h"
 
+//! counting triangles with enough GPU memory
 uint64_t GpuForward(int* edges, int num_nodes, uint64_t num_edges);
+//! counting large triangles which exceeds the GPU memory capacity
+//! by splitting the edges
 uint64_t GpuForwardSplit(int* edges, int num_nodes, uint64_t num_edges,
     int split_num = 2);
+//! get the minimum split number for current GPU device
 int GetSplitNum(int num_nodes, uint64_t num_edges);
