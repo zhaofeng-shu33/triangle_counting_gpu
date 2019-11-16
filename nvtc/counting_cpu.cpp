0fb6e92960e80b01294f3587164de0f7ab7b0b5b
zhaofeng-shu33 <616545598@qq.com>
2019-11-16 23:16:53 +0800

    fix not defined

--- nvtc/counting_cpu.cpp
+++ nvtc/counting_cpu.cpp
@@ -36,8 +36,6 @@ uint64_t CpuForward(int* edges, int node_num, uint64_t edge_num) {
 
 uint64_t CalculateTrianglesSplitCPU(int* edges, uint64_t* dev_nodes,
     uint64_t start_range, uint64_t end_range) {
-    uint64_t m = edge_num;
-    int n = node_num;
     // Calculate Triangles
     uint64_t count = 0;
 #pragma omp parallel for reduction(+:count)
