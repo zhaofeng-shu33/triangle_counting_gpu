4aa2c3b12008f4159caf5c09e5b7ade400f48f6a
zhaofeng-shu33 <616545598@qq.com>
2019-11-16 23:15:44 +0800

    fix line too long

--- nvtc/counting_cpu.cpp
+++ nvtc/counting_cpu.cpp
@@ -34,7 +34,8 @@ uint64_t CpuForward(int* edges, int node_num, uint64_t edge_num) {
     return count;
 }
 
-uint64_t CalculateTrianglesSplitCPU(int* edges, uint64_t* dev_nodes, uint64_t start_range, uint64_t end_range) {
+uint64_t CalculateTrianglesSplitCPU(int* edges, uint64_t* dev_nodes,
+    uint64_t start_range, uint64_t end_range) {
     uint64_t m = edge_num;
     int n = node_num;
     // Calculate Triangles
